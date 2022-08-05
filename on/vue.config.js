ee886f1ba970b5a563ed3319fbc644dd2e30dee1
Layne <Layne@fxy.com>
2022-08-05 11:51:37 +0800

    商城

---
+++ on/vue.config.js
@@ -0,0 +1,14 @@
+     module.exports=
+     {
+     // 关闭校验工具
+     lintOnSave:false,
+               //代理跨域
+               devServer: {
+               proxy: {
+                    '/api':{
+                         target:'http://gmall-h5-api.atguigu.cn'
+                    }
+
+               },
+               },
+     }
